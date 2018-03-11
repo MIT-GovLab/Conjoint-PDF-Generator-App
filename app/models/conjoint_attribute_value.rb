057c5339e4dc5491e6a71cc4d6f20a7bbccc575d
Alex Meyer <acmeyer9@gmail.com>
2018-03-11 16:51:41 -0400

    added aws region

--- app/models/conjoint_attribute_value.rb
+++ app/models/conjoint_attribute_value.rb
@@ -4,7 +4,8 @@ class ConjointAttributeValue < ActiveRecord::Base
   has_attached_file :image, 
                     styles: { medium: "300x300>", thumb: "100x100>" },
                     storage: :s3,
-                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+                    :s3_region => ENV['AWS_REGION']
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_presence_of :level
